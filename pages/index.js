cd643e6fa031b42d1943de956b770c57c9b7ccd0
Stedman Halliday <stedman.rh@gmail.com>
2023-09-02 17:37:10 -0700

    app install promo flow

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,76 @@ import styles from "../styles/Base.module.css";
 import secureLocalStorage from "react-secure-storage";
 
 export default function Home() {
+    // Initialize router
     const router = useRouter();
 
-    const [contact, setContact] = useState({
-        contactExists: false
+    // Initialize app state
+    const [appState, setAppState] = useState({
+        contactExists: false,
+        showInstallPrompt: false,
+        installPrompt: null
     }, []);
 
+    // Run on page load
     useEffect(() => {
+        // Shuffle text on loop
+        loopShuffle();
+
+        // Check if contact exists
         const formValues = JSON.parse(secureLocalStorage.getItem("formValues"));
         if (formValues != null) {
-            setContact({
-                contactExists: true
-            })
+            setAppState({ contactExists: true });
         }
+
+        // Check if app install prompt was shown
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent the mini-infobar from appearing on mobile
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            setAppState({
+                installPrompt: e,
+                showInstallPrompt: true
+            });
+        });
+
+        // Verify app installation
+        window.addEventListener('appinstalled', () => {
+            // Hide install promotion, discard prompt
+            setAppState({
+                showInstallPrompt: false,
+                installPrompt: null
+            })
+            // TODO: log install analytics
+            console.log('PWA was installed');
+        });
     }, [])
 
+    // App install prompt flow
+    const prompt = async () => {
+        // Hide install promotion
+        setAppState({ showInstallPrompt: false });
+        // Show install prompt
+        appState.installPrompt.prompt();
+        // Wait for the user to respond to the prompt ("accepted" | "dismissed")
+        const { outcome } = await appState.installPrompt.userChoice;
+        // TODO: log promo to prompt install analytics
+        console.log(`User ${outcome} install prompt`);
+        // Discard used prompt
+        setAppState({ installPrompt: null });
+    }
+
+    // Navigate to contact form
     const create = () => {
         router.push("/create");
     }
 
+    // Navigate to QR code
     const preview = () => {
         router.push("/preview");
     }
 
+    // Shuffle intro text
+    // TODO: long-running app causes incorrect strings
     const shuffle = () => {
         const words = ["seamlessly.", "flexibly.", "Tactfully."];
         let i = -1;
@@ -79,13 +126,13 @@ export default function Home() {
         animateOut(currentWord);
     };
 
-
-    useEffect(() => {
+    // Shuffle text on loop
+    const loopShuffle = () => {
         const shuffleTimeout = setTimeout(shuffle, 5000);
         return () => {
             clearTimeout(shuffleTimeout);
         }
-    }, []);
+    }
 
     return (
         <div>
@@ -95,9 +142,13 @@ export default function Home() {
                 <header className={styles.siteHeader}>
                     <p>Share your contact&nbsp;info <span id="shuffle" className={styles.shuffle}>Tactfully.</span></p>
                 </header>
-                {contact.contactExists ?
-                    (<button className="button" onClick={preview}>Share contact</button>) :
-                    (<button className="button" onClick={create}>+ New contact</button>)
+                {appState.contactExists ?
+                    <button className="button" onClick={preview}>Share contact</button>
+                    : <button className="button" onClick={create}>+ New contact</button>
+                }
+                {appState.showInstallPrompt ?
+                    <button className="button-txt" onClick={prompt}>⬇️ Add to home screen</button>
+                    : <button style={{ visibility: "hidden" }} className="button-txt" onClick={prompt}>⬇️ Add to home screen</button>
                 }
             </main>
         </div>
