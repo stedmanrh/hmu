2d3a0eebfc36ab737f313468dce684dee35e1384
Stedman Halliday <stedman.rh@gmail.com>
2023-09-09 10:36:09 -0700

    qr error correction

--- pages/preview.js
+++ pages/preview.js
@@ -61,7 +61,7 @@ export default function Preview() {
         QRCode.toDataURL(vCardValues(formValues),
             {
                 width: 168,
-                errorCorrectionLevel: 'H',
+                errorCorrectionLevel: 'L',
             }).then((url) => {
                 setDataUrl(url)
             });
