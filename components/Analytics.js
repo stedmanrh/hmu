23d4128f75ab9984e9d70227a5f5fb9e3be49cfd
VictorXLR <code@victorxlr.me>
2023-09-12 22:00:38 -0700

    Moved to Main

--- components/Analytics.js
+++ components/Analytics.js
@@ -2,11 +2,10 @@ import Script from "next/script";
 
 export default function Analytics(props) {
     // Google Analytics tag: G-0JWKW8VHW8
-
     return (
-        <div>
-            <Script src="https://www.googletagmanager.com/gtag/js?id=GA_G-0JWKW8VHW8" />
-            <Script id="google-analytics">
+        <div className="container">
+            <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=GA_G-0JWKW8VHW8" />
+            <Script strategy="lazyOnload" id="google-analytics">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/reset.css';
 import '../styles/globals.css';
+import Analytics from '../components/Analytics';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
