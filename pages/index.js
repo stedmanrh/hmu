f52779888ce3223b395926d3d8dc238da0d4f730
Stedman Halliday <stedman.rh@gmail.com>
2023-09-14 20:01:08 -0700

    event listener

--- pages/index.js
+++ pages/index.js
@@ -28,11 +28,13 @@ export default function Home() {
             showCursor: false
         });
 
-        // Check if contact data is stored
-        const formValues = JSON.parse(secureLocalStorage.getItem("formValues"));
-        if (formValues != null) {
-            setContactExists(true);
-        }
+        window.addEventListener("load", (e)=> {
+            // Check if contact data is stored
+            const formValues = JSON.parse(secureLocalStorage.getItem("formValues"));
+            if (formValues != null) {
+                setContactExists(true);
+            }
+        });
 
         return () => {
             // Destroy Typed instance during cleanup to stop animation
