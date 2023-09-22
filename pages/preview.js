e403d39f87924a8d40880e6b35285027c40129bc
Stedman Halliday <stedman.rh@gmail.com>
2023-09-21 22:18:31 -0700

    lint rules

--- pages/preview.js
+++ pages/preview.js
@@ -28,10 +28,10 @@ export default function Preview() {
         return vCard;
     }
 
-    const home = useCallback(()=> {
+    const home = () => {
         // location vs. router.push to fire beforeinstallprompt event
         router.push("/");
-    });
+    }
 
     const edit = () => {
         router.push("/create?editing=true");
