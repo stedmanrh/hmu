dc3f948238c8773820d73adcb4791da1f35813b0
Stedman Halliday <stedman.rh@gmail.com>
2023-09-23 11:34:47 -0700

    ios install instructions

--- components/InstallModal.js
+++ components/InstallModal.js
@@ -6,8 +6,8 @@ function Instructions({ os }) {
             <div className="text-base text-slate-600 space-y-3">
                 <ol className="ml-5 list-decimal space-y-3">
                     <li>Open <span className="text-purple-600">hmu.world</span> in Safari.</li>
-                    <li>Tap the <em className="relative font-medium uppercase tracking-wide pl-5 ml-1 mr-0.5 safariShare">share</em> button.</li>
-                    <li>Tap <em className="font-medium uppercase tracking-wide">Add to home screen</em>.</li>
+                    <li>Tap the <em className="relative font-medium uppercase tracking-wide pl-5 ml-1 mr-0.5 safariShare">share</em> button at the bottom of your screen.</li>
+                    <li>Scroll down and tap the <em className="font-medium uppercase tracking-wide">Add to home screen</em> option.</li>
                 </ol>
             </div>
         );
