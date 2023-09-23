acc1ddc36b3e92c249ab4661252f0c4fff1b4509
Stedman Halliday <stedman.rh@gmail.com>
2023-09-23 12:32:02 -0700

    link labels

--- components/LinkForm.js
+++ components/LinkForm.js
@@ -57,9 +57,9 @@ export default function LinkForm() {
     return (
         <form id="linkForm" name="Link form" className="w-full max-w-md flex flex-col px-2"
             onSubmit={handleSubmit}>
-            <Input name="instagram" label="Instagram username" type="text" value={formfield.instagram} placeholder="garyvee" onChange={handleChange} />
-            <Input name="twitter" label="X (Twitter) username" type="text" value={formfield.twitter} placeholder="garyvee" onChange={handleChange} />
-            <Input name="linkedin" label="LinkedIn username" type="text" value={formfield.linkedin} placeholder="garyvaynerchuk" onChange={handleChange} />
+            <Input name="instagram" label="Instagram" type="text" value={formfield.instagram} placeholder="garyvee" onChange={handleChange} />
+            <Input name="twitter" label="X (Twitter)" type="text" value={formfield.twitter} placeholder="garyvee" onChange={handleChange} />
+            <Input name="linkedin" label="LinkedIn" type="text" value={formfield.linkedin} placeholder="garyvaynerchuk" onChange={handleChange} />
             <Button type="submit" className="self-center my-4 shadow-none">Save links</Button>
             <TextButton onClick={cancel} className="self-center">Cancel</TextButton>
         </form>
