f8ea3351f2ac61482a65594cb4b5e504d382e9ed
Stedman Halliday <stedman.rh@gmail.com>
2023-10-31 18:52:20 -0700

    rm container classname analytics

--- components/Analytics.js
+++ components/Analytics.js
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function Analytics() {
     // Google Analytics tag: G-0JWKW8VHW8
     return (
-        <div className="container">
+        <div>
             <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=GA_G-0JWKW8VHW8" />
             <Script strategy="afterInteractive" id="gtag-init"
                 dangerouslySetInnerHTML={{__html: `
